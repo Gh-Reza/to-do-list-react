c268352516a1703a9f1099de4c08fcac356410cf
Reza Merzaie <ghrezam97@gmail.com>
2023-07-11 13:40:31 +0430

    Remove import App.css fro App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Header from './components/header';
